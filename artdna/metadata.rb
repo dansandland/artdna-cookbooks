c4ef9ed8d9f6f354c20e32071ac937bc243d0d88
Jackson-Sandland <jacksonsandland@gmail.com>
2015-04-27 20:54:46 -0700

    Add artdna library

---
+++ artdna/metadata.rb
@@ -0,0 +1,7 @@
+name             'artdna'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures artdna'
+long_description 'Installs/Configures artdna'
+version          '0.1.0'
